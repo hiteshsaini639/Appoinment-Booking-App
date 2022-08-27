f4be49966e7dc4a7392868e9e99653af2d7a6a0f
hitesh saini <hiteshsaini639@gmail.com>
2022-08-27 23:41:28 +0530

    Task 13-Final Product

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 "use strict"
 const myform=document.querySelector(".my-form");
-const fnameInput=document.querySelector("#fname");
-const lnameInput=document.querySelector("#lname");
+const nameInput=document.querySelector("#name");
+const emailInput=document.querySelector("#email");
 const userList=document.querySelector("#users");
 const msg=document.querySelector(".msg");
 const body=document.querySelector("body");
 const btn=document.querySelector(".btn");
+const btndel=document.querySelector('.del');
+const btnedit=document.querySelector('.edit');
 
 myform.addEventListener('mouseover',()=>{
     myform.classList.add("bg-dark");
@@ -18,15 +20,18 @@ myform.addEventListener('mouseout',()=>{
 // Add eventlistener to window
 window.addEventListener('load',loadLocalData);
 
+// delete
+userList.addEventListener('click',detetedata);
+
+// edit
+userList.addEventListener('click',editData);
+
 btn.addEventListener('click',submit);
 
-let number=localStorage.getItem('number');
-if(number==null)
-number=1;
 function submit(event){
     event.preventDefault();
 
-    if(fnameInput.value==='' || lnameInput.value===''){
+    if(nameInput.value==='' || emailInput.value===''){
         msg.classList.add("error");
         msg.textContent="Please enter all field";
         setTimeout(function(){
@@ -40,28 +45,61 @@ function submit(event){
         msg.textContent='';},1000);
 
         let obj=JSON.stringify({
-            firstName:fnameInput.value,
-            lastName:lnameInput.value
+            name:nameInput.value,
+            email:emailInput.value
         });
 
-        localStorage.setItem(`obj ${number}`,obj);
-        localStorage.setItem('number',`${++number}`);
-        fnameInput.value='';
-        lnameInput.value='';
+        localStorage.setItem(`${emailInput.value}`,obj);
+
+        nameInput.value='';
+        emailInput.value='';
     }
 
 }
 
 // Show localstorage data on reload
 function loadLocalData(){
-    let total=localStorage.getItem('number');
-    if(total!=null){
-        for(let i=1;i<total;i++){
-            let jsonData=localStorage.getItem(`obj ${i}`);
+        for(let i=0;i<localStorage.length;i++){
+            let jsonData=localStorage.getItem(localStorage.key(i));
             let data=JSON.parse(jsonData);
             let li=document.createElement('li');
-            li.appendChild(document.createTextNode(`${data.firstName} ${data.lastName}`));
+
+            let btndel =document.createElement('button');
+            let btnedit =document.createElement('button');
+
+            btndel.className='btn1 del';
+            btnedit.className='btn1 edit';
+
+            btndel.appendChild(document.createTextNode('Delete'));
+            btnedit.appendChild(document.createTextNode('Edit'));
+
+            li.appendChild(document.createTextNode(`Name:- ${data.name} Email:- ${data.email}`));
+            li.appendChild(btndel);
+            li.appendChild(btnedit);
             userList.appendChild(li);
+            
         }
+    }
+
+// Delete data
+function detetedata(e){
+    if(e.target.classList.contains('del')){
+        if(confirm('Are You Sure?')){
+            let textArray=e.target.parentElement.firstChild.textContent.split(' ');
+            localStorage.removeItem(textArray[textArray.length-1]);
+            userList.removeChild(e.target.parentElement);
+        }
+    }
+}
+
+// Edit data
+function editData(e){
+    if(e.target.classList.contains('edit')){
+        let textArray=e.target.parentElement.firstChild.textContent.split(' ');
+        let obj=JSON.parse(localStorage.getItem(textArray[textArray.length-1]));
+        nameInput.value=obj.name;
+        emailInput.value=obj.email;
+        localStorage.removeItem(textArray[textArray.length-1]);
+        userList.removeChild(e.target.parentElement);
     }
 }
