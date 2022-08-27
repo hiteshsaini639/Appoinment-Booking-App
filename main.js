90a3129aca484e21bef727bd4883945c0971e977
hitesh saini <hiteshsaini639@gmail.com>
2022-08-27 21:46:22 +0530

    Task 11-Storing in localStorage

--- main.js
+++ main.js
@@ -15,8 +15,14 @@ myform.addEventListener('mouseout',()=>{
     myform.classList.remove("bg-dark")
 });
 
+// Add eventlistener to window
+window.addEventListener('load',loadLocalData);
+
 btn.addEventListener('click',submit);
-let number=1;
+
+let number=localStorage.getItem('number');
+if(number==null)
+number=1;
 function submit(event){
     event.preventDefault();
 
@@ -33,10 +39,30 @@ function submit(event){
         setTimeout(()=>{msg.classList.remove('success');
         msg.textContent='';},1000);
 
-        localStorage.setItem(`First name ${number}`,`${fnameInput.value}`);
-        localStorage.setItem(`Last Name ${number}`,`${lnameInput.value}`);
-        ++number;
+        let obj=JSON.stringify({
+            firstName:fnameInput.value,
+            lastName:lnameInput.value
+        });
+
+        localStorage.setItem(`obj ${number}`,obj);
+        localStorage.setItem('number',`${++number}`);
         fnameInput.value='';
         lnameInput.value='';
+    }
+
+}
+
+// Show localstorage data on reload
+function loadLocalData(){
+    let total=localStorage.getItem('number');
+    if(number!=null){
+        for(let i=1;i<total;i++){
+            let jsonData=localStorage.getItem(`obj ${i}`);
+            // if(jsonData==null) break;
+            let data=JSON.parse(jsonData);
+            let li=document.createElement('li');
+            li.appendChild(document.createTextNode(`${data.firstName} ${data.lastName}`));
+            userList.appendChild(li);
+        }
     }
 }
