14a4901ad6fe54593f1688fb589723470ced1932
hitesh saini <hiteshsaini639@gmail.com>
2022-09-06 15:38:19 +0530

    PUT data

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const body = document.querySelector("body");
 const btn = document.querySelector(".btn");
 const btndel = document.querySelector(".del");
 const btnedit = document.querySelector(".edit");
+const heading = document.querySelector("form h2");
+let editing = false;
+let editingLi;
+const APIendpoint = "https://crudcrud.com/api/26d25124b882440693f15e1977744889";
 
 myform.addEventListener("mouseover", () => {
   myform.classList.add("bg-dark");
@@ -18,13 +22,13 @@ myform.addEventListener("mouseout", () => {
 });
 
 // for refreshing the page
-window.addEventListener("load", getAPIcall);
+window.addEventListener("DOMContentLoaded", getDataOnLoad);
 
 // delete
 userList.addEventListener("click", deteteData);
 
 // edit
-//userList.addEventListener("click", editData);
+userList.addEventListener("click", editData);
 
 // submit
 btn.addEventListener("click", submit);
@@ -41,10 +45,11 @@ function showMsg(message, type) {
 
 // show output on frontend
 function showData(obj) {
-  const createTextNode = `<li class="listItem" id="${obj._id}">Name: ${obj.name} Email: ${obj.email}<button class="btn1 del">Delete</button><button class="btn1 edit">Edit</button></li>`;
+  const createTextNode = `<li class="listItem" id="${obj._id}" name="${obj.name}" email="${obj.email}" >Name: ${obj.name} Email: ${obj.email}<button class="btn1 del">Delete</button><button class="btn1 edit">Edit</button></li>`;
   userList.innerHTML += createTextNode;
 }
 
+//submit
 function submit(event) {
   event.preventDefault();
 
@@ -52,35 +57,46 @@ function submit(event) {
   if (nameInput.value === "" || emailInput.value === "") {
     showMsg("Please enter all field ❌", "error");
   } else {
-    // if user input duplicate email id
-    let liArray = document.querySelectorAll(".listItem");
-    liArray.forEach((li) => {
-      let liTextArray = li.firstChild.textContent.split(" ");
-      if (liTextArray[liTextArray.length - 1] == emailInput.value) {
-        deleteDataFromFrontAndBack(li)
-          .then(() => showMsg("User data replaced", "success"))
-          .catch((err) => showMsg(err.message, "error"));
-      }
-    });
-
     // creating object from user detail
     let obj = {
       name: nameInput.value,
       email: emailInput.value,
     };
 
-    // Sending data to backend API
-    axios
-      .post(
-        "https://crudcrud.com/api/128c6fbbcaa84272b0df3724c1c7b624/UserDetails",
-        obj
-      )
-      .then((res) => {
-        // showing success messege on submitting
-        showMsg("User added ✅", "success");
-        showData(res.data);
-      })
-      .catch((err) => showMsg(err.message, "error"));
+    // if user input duplicate email id
+    let liArray = document.querySelectorAll(".listItem");
+    liArray.forEach((li) => {
+      if (li.getAttribute("email") == emailInput.value) {
+        editing = true;
+        editingLi = li;
+      }
+    });
+
+    if (editing) {
+      //Edit User
+      axios
+        .put(`${APIendpoint}/UserDetails/${editingLi.id}`, obj)
+        .then((res) => {
+          // showing success messege on submitting
+          showMsg("User Edited ✅", "success");
+          editingLi.innerHTML = `Name: ${obj.name} Email: ${obj.email}<button class="btn1 del">Delete</button><button class="btn1 edit">Edit</button>`;
+          editingLi.setAttribute("name", obj.name);
+          editingLi.setAttribute("email", obj.email);
+          editing = false;
+          heading.innerHTML = "Add User";
+        })
+        .catch((err) => showMsg(err.message, "error"));
+    } else {
+      //Add user
+      axios
+        .post(`${APIendpoint}/UserDetails`, obj)
+        .then((res) => {
+          // showing success messege on submitting
+          showMsg("User added ✅", "success");
+          showData(res.data);
+        })
+        .catch((err) => showMsg(err.message, "error"));
+    }
 
     // clearing data
     nameInput.value = "";
@@ -88,26 +104,24 @@ function submit(event) {
   }
 }
 
-// Show API call data on reload
-function getAPIcall() {
+// Show data on reload
+function getDataOnLoad() {
   axios
-    .get(
-      "https://crudcrud.com/api/128c6fbbcaa84272b0df3724c1c7b624/UserDetails"
-    )
+    .get(`${APIendpoint}/UserDetails`)
     .then((res) => {
       for (let i = 0; i < res.data.length; i++) {
         showData(res.data[i]);
       }
     })
     .catch((err) => showMsg(err.message, "error"));
+  editing = false;
+  heading.innerHTML = "Add User";
 }
 
-// Delete data from frontend
+// Delete data from frontend and backend
 function deleteDataFromFrontAndBack(li) {
   userList.removeChild(li);
-  return axios.delete(
-    `https://crudcrud.com/api/128c6fbbcaa84272b0df3724c1c7b624/UserDetails/${li.id}`
-  );
+  return axios.delete(`${APIendpoint}/UserDetails/${li.id}`);
 }
 
 // Delete data
@@ -121,14 +135,13 @@ function deteteData(e) {
   }
 }
 
-// Edit data
-// function editData(e) {
-//   if (e.target.classList.contains("edit")) {
-//     let textArray = e.target.parentElement.firstChild.textContent.split(" ");
-//     let obj = JSON.parse(localStorage.getItem(textArray[textArray.length - 1]));
-//     nameInput.value = obj.name;
-//     emailInput.value = obj.email;
-//     localStorage.removeItem(textArray[textArray.length - 1]);
-//     userList.removeChild(e.target.parentElement);
-//   }
-// }
+//Edit data
+function editData(e) {
+  if (e.target.classList.contains("edit")) {
+    editingLi = e.target.parentElement;
+    editing = true;
+    heading.innerHTML = "Edit User";
+    nameInput.value = editingLi.getAttribute("name");
+    emailInput.value = editingLi.getAttribute("email");
+  }
+}
